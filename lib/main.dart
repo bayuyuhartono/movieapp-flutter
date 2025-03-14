dc97cf9e06d2691d8be8095ffcd731cbbbfe6531
bayuyuhartono <bayuyuhartono@gmail.com>
2025-03-14 19:29:49 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp/src/view/home_screen.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/src/view/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: 
+      Text(
+        "Movie App", 
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          )
+      )),
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
