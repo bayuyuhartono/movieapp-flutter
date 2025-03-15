fe783a2eaa0c362be9cb1783b1569f63c2c664f0
bayuyuhartono <bayuyuhartono@gmail.com>
2025-03-15 16:41:38 +0700

    feat: botom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movieapp/src/view/home_screen.dart';
+import 'package:movieapp/src/view/bottom_navigation.dart';
 
 void main() {
   runApp(MainApp());
@@ -20,7 +20,23 @@ class MainApp extends StatelessWidget {
       ),
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: BottomNavigation()
     );
   }
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     theme: ThemeData(
+  //       useMaterial3: true,
+  //       colorSchemeSeed: const Color.fromRGBO(86, 80, 14, 171),
+  //     ),
+  //     darkTheme: ThemeData(
+  //       useMaterial3: true,
+  //       brightness: Brightness.dark,
+  //       colorSchemeSeed: const Color.fromRGBO(86, 80, 14, 171),
+  //     ),
+  //     themeMode: ThemeMode.dark,
+  //     debugShowCheckedModeBanner: false,
+  //     home: HomeScreen(),
+  //   );
+  // }
 }
