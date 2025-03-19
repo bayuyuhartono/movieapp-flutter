5977875c033042458a14ed314fffb6dc0ee578a8
bayuyuhartono <bayuyuhartono@gmail.com>
2025-03-20 04:20:34 +0700

    feat: firebase and crashlitycs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_addict/src/view/bottom_navigation.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  
   runApp(MainApp());
 }
 
